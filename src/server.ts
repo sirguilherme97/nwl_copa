7aa6ea2961e6b37674d2b1bec43aacb7b84212ab
SirGuilhermeBR <sir_guilherme@hotmail.com>
2022-11-02 17:04:36 -0300

    Start Project - NLW COPA

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import Fastify from "fastify";
+
+async function bootstrap() {
+    const fastify = Fastify({
+        logger: true,
+    })
+
+    await fastify.listen({ port: 3333 })
+}
+
+bootstrap()
